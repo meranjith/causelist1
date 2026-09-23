d0cac16e1fa79ebbeb669d99d09ef679331fe916
ranjith <myselfabstar2001@gmail.com>
2026-09-23 23:52:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pdfplumber
 
 with pdfplumber.open("input/Mahesh_Chowdhary.pdf") as pdf:
-    page = pdf.pages[0]
 
-    italic_text = ""
+    for page_no, page in enumerate(pdf.pages, start=1):
 
-    for ch in page.chars:
-        if "Italic" in ch["fontname"]:
-            italic_text += ch["text"]
+        italic_text = ""
 
-    print(italic_text)
+        for ch in page.chars:
+
+            if "Italic" in ch["fontname"]:
+                italic_text += ch["text"]
+
+        print("\n")
+        print("=" * 80)
+        print("PAGE", page_no)
+        print("=" * 80)
+        print(italic_text)
