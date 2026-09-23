2301860b0dedeca0c0a443ded70c14144f657cea
ranjith <myselfabstar2001@gmail.com>
2026-09-23 16:41:26 +0530

    Update test_extract.py

--- test_extract.py
+++ test_extract.py
@@ -1,14 +1,18 @@
 import pdfplumber
 
-pdf_file = "input/Mahesh_Chowdhary.pdf"
+pdf_file="Mahesh_Chowdhary.pdf"
 
 with pdfplumber.open(pdf_file) as pdf:
-    for i, page in enumerate(pdf.pages):
-        print("=" * 100)
-        print(f"PAGE {i+1}")
-        print("=" * 100)
+    for page_no,page in enumerate(pdf.pages,1):
 
-        text = page.extract_text()
+        print(f"\nPAGE {page_no}\n")
 
-        if text:
-            print(text)
+        tables=page.extract_tables()
+
+        print("TABLE COUNT:",len(tables))
+
+        for i,t in enumerate(tables,1):
+            print("\nTABLE",i)
+
+            for row in t[:10]:
+                print(row)
