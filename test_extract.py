cfe03d421eb0bfa6dea83900714f85457864a1ad
ranjith <myselfabstar2001@gmail.com>
2026-09-23 13:39:58 +0530

    Create test_extract.py

---
+++ test_extract.py
@@ -0,0 +1,14 @@
+import pdfplumber
+
+pdf_file = "Mahesh_Chowdhary.pdf"
+
+with pdfplumber.open(pdf_file) as pdf:
+    for i, page in enumerate(pdf.pages):
+        print("=" * 100)
+        print(f"PAGE {i+1}")
+        print("=" * 100)
+
+        text = page.extract_text()
+
+        if text:
+            print(text)
