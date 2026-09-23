c8dea9823ad90035b8e14363e257e8243c063005
ranjith <myselfabstar2001@gmail.com>
2026-09-23 23:44:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import pdfplumber
 
 with pdfplumber.open("input/Mahesh_Chowdhary.pdf") as pdf:
-
     page = pdf.pages[0]
 
-    count = 0
+    italic_text = ""
 
     for ch in page.chars:
-
         if "Italic" in ch["fontname"]:
+            italic_text += ch["text"]
 
-            print(ch["text"], ch["fontname"])
-
-            count += 1
-
-            if count > 200:
-                break
+    print(italic_text)
